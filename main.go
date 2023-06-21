5a27d542f876f411278e2676388a8a7a696c28bc
Zhandos <zhandos.zhandarbekov@gmail.com>
2023-06-21 18:49:51 +0600

    added ico

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
@@ -9,13 +11,27 @@ import (
 func main() {
 	a := app.New()
 	w := a.NewWindow("Первое приложнеие")
+	label := widget.NewLabel("Введите IP терминала ")
 
-	label := widget.NewLabel("Привет")
+	w.Resize(fyne.NewSize(400, 320))
+
+	entry := widget.NewEntry()
+
+	btn := widget.NewButton("Сохранить", func() {
+		ip := entry.Text
+
+		fmt.Println(ip)
+	})
 
 	w.SetContent(container.NewVBox(
-		label, widget.NewButton("hi", func() {
-			label.SetText("world")
-		})))
+		entry,
+		label,
+		btn,
+	))
 
 	w.ShowAndRun()
+}
+
+func btnHandler() {
+
 }
