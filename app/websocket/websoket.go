977b499bdd66e58ee6f245f6837211cf7bbb77d6
Zhandos <zhandos.zhandarbekov@gmail.com>
2023-06-22 10:00:42 +0600

    added ico

--- app/websocket/websoket.go
+++ app/websocket/websoket.go
@@ -1,36 +1,39 @@
-package websoket
+package websocket
 
 import (
 	"fmt"
 	"net/http"
 )
 
-http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
-	conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
+func Server() {
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
 
-	for {
-		// Read message from browser
-		msgType, msg, err := conn.ReadMessage()
-		if err != nil {
-			return
+		for {
+			// Read message from browser
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
+
+			// Print the message to the console
+			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+
+			// Write message back to browser
+			if err = conn.WriteMessage(msgType, msg); err != nil {
+				return
+			}
 		}
+	})
 
-		// Print the message to the console
-		fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "websockets.html")
+	})
 
-		// Write message back to browser
-		if err = conn.WriteMessage(msgType, msg); err != nil {
-			return
-		}
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		return
 	}
-})
 
-http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "websockets.html")
-})
-
-err := http.ListenAndServe(":8080", nil)
-
-if err != nil {
-return
 }
--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gorilla/websocket"
-	window "github.com/zhandos717/gateway/app"
-	"net/http"
+	"github.com/zhandos717/gateway/app/window"
 )
 
 var upgrader = websocket.Upgrader{
@@ -15,34 +13,4 @@ var upgrader = websocket.Upgrader{
 func main() {
 
 	window.Run()
-
-	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
-		conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
-
-		for {
-			// Read message from browser
-			msgType, msg, err := conn.ReadMessage()
-			if err != nil {
-				return
-			}
-
-			// Print the message to the console
-			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
-
-			// Write message back to browser
-			if err = conn.WriteMessage(msgType, msg); err != nil {
-				return
-			}
-		}
-	})
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "websockets.html")
-	})
-
-	err := http.ListenAndServe(":8080", nil)
-
-	if err != nil {
-		return
-	}
 }
